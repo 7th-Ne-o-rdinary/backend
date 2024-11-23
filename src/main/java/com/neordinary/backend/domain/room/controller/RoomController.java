171bb57f59a7e62df2895435d4eacd083e513471
SeungGyu <phillipskl123@gmail.com>
2024-11-23 22:28:45 +0900

    feat: 방 참여기능 구현

--- src/main/java/com/neordinary/backend/domain/room/controller/RoomController.java
+++ src/main/java/com/neordinary/backend/domain/room/controller/RoomController.java
@@ -6,6 +6,7 @@ import com.neordinary.backend.domain.user.domain.User;
 import com.neordinary.backend.global.jwt.CurrentUser;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,12 +15,23 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class RoomController {
 
+    private static final String JOIN_MESSAGE = "Joined room Success";
+
     private final RoomService roomService;
+
 
     @PostMapping
     @PreAuthorize("isAuthenticated()")
     @SecurityRequirement(name = "access-token")
     public String create(@CurrentUser User user, @RequestBody final RequestCreateRoom requestCreateRoom) {
         return roomService.create(user, requestCreateRoom);
+    }
+
+    @PostMapping("/join")
+    @PreAuthorize("isAuthenticated()")
+    @SecurityRequirement(name = "access-token")
+    public ResponseEntity<String> join(@CurrentUser User user, @RequestParam("code") String code){
+        roomService.join(user, code);
+        return ResponseEntity.ok(JOIN_MESSAGE);
     }
 }
--- src/main/java/com/neordinary/backend/domain/room/repository/RoomRepository.java
+++ src/main/java/com/neordinary/backend/domain/room/repository/RoomRepository.java
@@ -3,5 +3,8 @@ package com.neordinary.backend.domain.room.repository;
 import com.neordinary.backend.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findByCode(String code);
 }

edf696964aa9509d8e1dd391c43362a040a9596c
김민규 <min1925k@gmail.com>
2024-11-23 23:08:48 +0900

    fix: import 추가

--- src/main/java/com/neordinary/backend/domain/room/repository/RoomRepository.java
+++ src/main/java/com/neordinary/backend/domain/room/repository/RoomRepository.java
@@ -3,6 +3,7 @@ package com.neordinary.backend.domain.room.repository;
 
 import java.util.Optional;
 
+import com.neordinary.backend.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 

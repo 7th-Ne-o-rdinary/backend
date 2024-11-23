4febb90682a16b30e571569d12ec7131aeab4337
Kim So Yeon <qws1566@naver.com>
2024-11-24 03:49:35 +0900

    투표 참가자 수정

--- src/main/java/com/neordinary/backend/domain/vote/service/VoteService.java
+++ src/main/java/com/neordinary/backend/domain/vote/service/VoteService.java
@@ -2,6 +2,7 @@ package com.neordinary.backend.domain.vote.service;
 
 import java.util.List;
 
+import com.neordinary.backend.domain.user.domain.User;
 import com.neordinary.backend.domain.vote.dto.VoteRequestDto;
 import com.neordinary.backend.domain.vote.dto.VoteResponseDto;
 import com.neordinary.backend.domain.vote.dto.VoteResultDto;
@@ -11,5 +12,5 @@ public interface VoteService {
 
 
 
-	VoteResponseDto.createVoteResultDto createVote(VoteRequestDto.createVoteDto request);
+	VoteResponseDto.createVoteResultDto createVote(User user,VoteRequestDto.createVoteDto request);
 }

a88c57d46e63f336a42ce5d5750b8331cfa96f9a
SeungGyu <phillipskl123@gmail.com>
2024-11-24 03:04:22 +0900

    refactor: question List 호출 api 수정

--- src/main/java/com/neordinary/backend/domain/question/service/QuestionServiceImpl.java
+++ src/main/java/com/neordinary/backend/domain/question/service/QuestionServiceImpl.java
@@ -5,35 +5,23 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import com.neordinary.backend.domain.question.converter.QuestionConverter;
-import com.neordinary.backend.domain.question.dto.QuestionRequestDto;
 import com.neordinary.backend.domain.question.dto.QuestionResponseDto;
-import com.neordinary.backend.domain.question.entity.Question;
-import com.neordinary.backend.domain.question.repository.QuestionRepository;
 import com.neordinary.backend.domain.room.entity.Room;
 import com.neordinary.backend.domain.room.repository.RoomRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionServiceImpl implements QuestionService{
-	private final QuestionRepository questionRepository;
+
 	private final RoomRepository roomRepository;
-	@Override
-	public  QuestionResponseDto.CreateQuestionResultDto createQuestion(QuestionRequestDto request, Long roomId){
-		Question question = QuestionConverter.toQuestion(request,roomId);
-		questionRepository.save(question);
-		return QuestionConverter.createQuestionResultDto(question);
-
-	}
-	@Override
-	public void deleteQuestion(Long questionId){
-		questionRepository.deleteById(questionId);
-	}
 
 	@Override
-	public QuestionResponseDto.questionListDto getQuestionList(Long roomId){
-		Optional<Room> room= roomRepository.findById(roomId);
+	@Transactional
+	public QuestionResponseDto.questionListDto getQuestionList(String code){
+		Optional<Room> room = roomRepository.findByCode(code);
 		return QuestionConverter.toQuestionList(room);
 	}
 }

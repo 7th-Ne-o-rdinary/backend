173064dbfbe2cc21c8be705f5ec69e7de60abee4
김민규 <min1925k@gmail.com>
2024-11-24 03:41:11 +0900

    feat: 참가자 조회 API 반환값 변경

--- src/main/java/com/neordinary/backend/domain/participant/dto/ParticipantDto.java
+++ src/main/java/com/neordinary/backend/domain/participant/dto/ParticipantDto.java
@@ -1,15 +1,20 @@
 package com.neordinary.backend.domain.participant.dto;
 
 import com.neordinary.backend.domain.participant.entity.Participant;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ParticipantDto {
 	private String name;
 
+	private String email;
+
 	public static ParticipantDto fromEntity(Participant participant) {
-		ParticipantDto participantDto = new ParticipantDto();
-		participantDto.setName(participant.getUser().getName());
-		return participantDto;
+		return ParticipantDto.builder()
+				.name(participant.getUser().getName())
+				.email(participant.getUser().getEmail())
+				.build();
 	}
 }

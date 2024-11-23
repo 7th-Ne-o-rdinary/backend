0935222a63a2aa1703645ecda213b95de8246904
김민규 <min1925k@gmail.com>
2024-11-23 21:51:55 +0900

    feat: cicd yml 주입 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,7 +11,7 @@ echo "[$NOW] $JAR 복사" >> $START_LOG
 cp "$ROOT_PATH/build/libs/backend.jar" "$JAR"
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -Dspring.profiles.active=prod -Duser.timezone=Asia/Seoul -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+nohup java -Duser.timezone=Asia/Seoul -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
